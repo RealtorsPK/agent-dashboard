1d0e9d593320ea6c74b9853bda8fbcba846dfbf5
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-09 16:45:19 +0500

    tabs

--- components/ui/tabs/index.jsx
+++ components/ui/tabs/index.jsx
@@ -3,11 +3,16 @@ const Tabs = ({ options, value, onChange }) => (
     {
       options && options.length > 0 ?
         options.map((items) => (
-          <button className={`text-[12px] font-medium p-[6px_15px] border-[1px] border-border-secondary rounded-[5px] ${value === items.value ? 'bg-background-primary text-white' : 'bg-white text-text-tertiary '}`} key={items.label} onClick={() => onChange(items.value)} type='button'>
+          <button
+              className={`text-[12px] font-medium p-[6px_15px] border-[1px] border-border-secondary rounded-[5px] ${value === items.value ? 'bg-background-primary text-white' : 'bg-white text-text-tertiary '}`}
+              key={items.label}
+              onClick={() => onChange(items.value)}
+              type="button"
+          >
             {items.label}
           </button>
         ))
-        : null
+        : undefined
     }
   </div>
 );
