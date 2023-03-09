0a76fc976354f94e231664bb9942ec861c0838dc
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-09 16:41:40 +0500

    eslint changes

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'get-off-my-lawn',
+  rules: {
+    // enable additional rules, override rule options, or disable rules
+    "indent": "off",
+    "react/jsx-indent": "off",
+    "object-curly-spacing": "off",
+    "unicorn/no-nested-ternary": "off",
+    "import/no-unresolved": "off",
+    "brace-style": "off",
+    "react/jsx-indent-props": "off",
+  },
+};
+
