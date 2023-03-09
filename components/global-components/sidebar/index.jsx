0aff9d365999450765e75c66d1d8cf4bc401159a
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-09 16:42:46 +0500

    index of global component

--- components/global-components/sidebar/index.jsx
+++ components/global-components/sidebar/index.jsx
@@ -1,6 +1,7 @@
-import { useRouter } from "next/router";
-import { useState } from "react";
-import buttons from "./buttons";
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
+import { buttons } from './buttons';
 
 const SideBar = () => {
   const routes = useRouter();
@@ -9,22 +10,29 @@ const SideBar = () => {
 
   const goToUrl = (url, item) => {
     routes.push(url);
+
     if (item) {
-      setActive(item.url)
+      setActive(item.url);
     }
-  }
+  };
 
   const toggleDropdownFun = (item) => {
-    setToggleDropdown(item.title)
-  }
-
+    setToggleDropdown(item.title);
+  };
 
   return (
     <div className="bg-white min-h-[calc(100vh-60px)] py-[20px]">
       {
         buttons.map((items) => (
-          <div className={`${toggleDropdown === items.title ? '' : 'h-[41px]'} overflow-hidden transition ease-in-out duration-500`} key={items.title}>
-            <button className={`block w-full text-left text-[14px] py-[10px] px-[20px] ${items.url === active ? 'text-[#000] font-medium' : 'text-[#888888]'}`} onClick={() => items.subMenu ? toggleDropdownFun(items) : goToUrl(items.url, items)} type='button'>
+          <div
+              className={`${toggleDropdown === items.title ? '' : 'h-[41px]'} overflow-hidden transition ease-in-out duration-500`}
+              key={items.title}
+          >
+            <button
+                className={`block w-full text-left text-[14px] py-[10px] px-[20px] ${items.url === active ? 'text-[#000] font-medium' : 'text-[#888888]'}`}
+                onClick={() => items.subMenu ? toggleDropdownFun(items) : goToUrl(items.url, items)}
+                type="button"
+            >
               {items.title}
             </button>
 
@@ -32,9 +40,15 @@ const SideBar = () => {
               items.subMenu ?
                 items.subMenu.map((subItems) => (
                   <div key={subItems.title}>
-                    <button className="block w-full text-left text-[14px] mb-[10px] px-[20px] ml-[15px] text-[#888888]" onClick={() => goToUrl(subItems.url, items)} type='button'>{subItems.title}</button>
+                    <button
+                        className="block w-full text-left text-[14px] mb-[10px] px-[20px] ml-[15px] text-[#888888]"
+                        onClick={() => goToUrl(subItems.url, items)}
+                        type="button"
+                    >
+                      {subItems.title}
+                    </button>
                   </div>
-                )) : null
+                )) : undefined
             }
           </div>
         ))
