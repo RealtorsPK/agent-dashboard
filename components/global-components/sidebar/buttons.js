5b4fc127171623b61f914789af4fdc3f41ffbca9
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-09 16:42:36 +0500

    sidebar button

--- components/global-components/sidebar/buttons.js
+++ components/global-components/sidebar/buttons.js
@@ -1,49 +1,83 @@
-
-export const buttons = [
-
+const buttons = [
   // ***************** Dahboard
-  { title: 'Dashboard', url: '/dashboard', imgUrl: null, subMenu: null, activePages: ['dashboard'] },
+  {
+    activePages: ['dashboard'],
+    imgUrl: undefined,
+    subMenu: undefined,
+    title: 'Dashboard',
+    url: '/dashboard',
+  },
 
   // ***************** Commercial
   {
-    title: 'Commercial', url: '/dashboard/properties/commercial/listing', imgUrl: null,
+    activePages: ['Commercial'],
+    imgUrl: undefined,
     subMenu: [
-      { title: 'Properties', url: '/dashboard/properties/commercial/listing', imgUrl: null, subMenu: [], activePages: ['/properties', '/addProperties', '/editProperty'] },
-      { title: 'Leads', url: '/dashboard/leads/commercial-leads', imgUrl: null, subMenu: null, activePages: [''], },
+      {
+        activePages: ['/properties', '/addProperties', '/editProperty'],
+        imgUrl: undefined,
+        subMenu: [],
+        title: 'Properties',
+        url: '/dashboard/properties/commercial/listing',
+      },
+      {
+        activePages: [''],
+        imgUrl: undefined,
+        subMenu: undefined,
+        title: 'Leads',
+        url: '/dashboard/leads/commercial-leads',
+      },
     ],
-    activePages: ['Commercial']
+    title: 'Commercial',
+    url: '/dashboard/properties/commercial/listing',
   },
 
   // ***************** Residential
   {
-    title: 'Residential', url: '/dashboard/properties/residential/listing', imgUrl: null,
-
+    activePages: ['Residential'],
+    imgUrl: undefined,
     subMenu: [
-      { title: 'Properties', url: '/dashboard/properties/residential/listing', imgUrl: null, subMenu: [], activePages: ['/dashboard/properties/residential', '/add-residential-properties', '/edit-residential-property'] },
-      { title: 'Leads', url: '/dashboard/leads/residential-leads', imgUrl: null, subMenu: null, activePages: ['/residential-leads'], },
+      {
+        activePages: ['/dashboard/properties/residential', '/add-residential-properties', '/edit-residential-property'],
+        imgUrl: undefined,
+        subMenu: [],
+        title: 'Properties',
+        url: '/dashboard/properties/residential/listing',
+      },
+      {
+        activePages: ['/residential-leads'],
+        imgUrl: undefined,
+        subMenu: undefined,
+        title: 'Leads',
+        url: '/dashboard/leads/residential-leads',
+      },
     ],
-
-    activePages: ['Residential']
+    title: 'Residential',
+    url: '/dashboard/properties/residential/listing',
   },
 
   // ***************** Agency
-  { title: 'Agency', url: '/dashboard/agency', imgUrl: null, subMenu: null, activePages: ['Agency'] }
+  {
+    activePages: ['Agency'],
+    imgUrl: undefined,
+    subMenu: undefined,
+    title: 'Agency',
+    url: '/dashboard/agency',
+  },
 ];
 
-export const allPages = [
+const allPages = [
   ...buttons,
-
-  { title: 'Commercial', url: '/addProperties' },
-  { title: 'Commercial', url: '/editProperty' },
-  { title: 'Commercial', url: '/leads' },
-
-  { title: 'Residential', url: '/add-residential-properties' },
-  { title: 'Residential', url: '/edit-residential-property' },
-  { title: 'Residential', url: '/residential-leads' },
-
-  { title: 'Agency', url: '/agency' },
-
-
+  // { title: 'Commercial', url: '/addProperties' },
+  {
+    title: 'Agency',
+    url: '/agency',
+  },
 ];
 
-export default buttons
+export default buttons;
+
+export {
+  buttons,
+  allPages,
+};
