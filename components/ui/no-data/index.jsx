fa3bc9103579c8a9c6391712eeac3b16bc789ca1
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-09 16:44:57 +0500

    no data

--- components/ui/no-data/index.jsx
+++ components/ui/no-data/index.jsx
@@ -1,8 +1,15 @@
-import Image from "next/image";
-import { noData } from "../../../static-img-urls";
+import Image from 'next/image';
+
+import { noData } from '../../../static-img-urls';
 
 export const NoData = ({ className }) => (
   <span className="inline-block relative w-[200px] h-[200px]">
-    <Image alt="No Data Found" className={className || ''} fill sizes='200px' src={noData} />
+    <Image
+        alt="No Data Found"
+        className={className || ''}
+        fill
+        sizes="200px"
+        src={noData}
+    />
   </span>
-)
+);
