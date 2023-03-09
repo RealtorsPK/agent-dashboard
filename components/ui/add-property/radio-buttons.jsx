3123e433a962d25cca044a510d23e349237aa04c
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-09 16:43:54 +0500

    add property readio button

--- components/ui/add-property/radio-buttons.jsx
+++ components/ui/add-property/radio-buttons.jsx
@@ -1,10 +1,22 @@
-import Radio from "../Input/radio";
+import Radio from '../Input/radio';
 
 const RadioButtons = ({ onChange, selectedType }) => (
   <div className="flex flex-wrap gap-x-[20px]">
-    <Radio checked={selectedType === 'commercial'} label='Commercial' name='propertyType' onChange={onChange} value='commercial' />
-    <Radio checked={selectedType === 'residential'} label='Residential' name='propertyType' onChange={onChange} value='residential' />
+    <Radio
+        checked={selectedType === 'commercial'}
+        label="Commercial"
+        name="propertyType"
+        onChange={onChange}
+        value="commercial"
+    />
+    <Radio
+        checked={selectedType === 'residential'}
+        label="Residential"
+        name="propertyType"
+        onChange={onChange}
+        value="residential"
+    />
   </div>
-)
+);
 
 export default RadioButtons;
