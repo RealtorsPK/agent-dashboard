e5574e2d6c33b3b52206f779ed6dc2d2de2bae27
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-09 16:44:44 +0500

    loader

--- components/ui/loader/index.jsx
+++ components/ui/loader/index.jsx
@@ -1,4 +1,4 @@
 
-export const Loader = ({ className }) => (
-  <span className={`animate-spin inline-block w-[15px] h-[15px] border-[2px] rounded-[50%] ${className ? className : 'border-border-secondary'} border-r-[transparent]`} />
-)
+export const Loader = ({ className }) =>
+  <span className={`animate-spin inline-block w-[15px] h-[15px] border-[2px] rounded-[50%] ${className ? className : 'border-border-secondary'} border-r-[transparent]`} />;
+
