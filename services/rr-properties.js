8546c398c762ca2116b31281c53b4e5b041fc7d3
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-09 16:46:13 +0500

    rr properties

---
+++ services/rr-properties.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// Fetch all Properties
+export const fetchResidentialProperties = (query) => axios.get(`${process.env.NEXT_PUBLIC_API_URL}/rr-property/by-agent${query || ''}`);
+
+// Add Property
+export const addRrProperties = (payload) => axios.post(`${process.env.NEXT_PUBLIC_API_URL}/rr-property`, payload);
+
+// Get Property by id
+export const getPropertyById = (id) => axios.get(`${process.env.NEXT_PUBLIC_API_URL}/rr-property/by-agent/${id}`);
+
+// Update Property
+export const updateProperty = (id, payload) => axios.put(`${process.env.NEXT_PUBLIC_API_URL}/rr-property/${id}`, payload);
+
+// Delete Property
+export const propertyPublished = (id, payload) => axios.patch(`${process.env.NEXT_PUBLIC_API_URL}/rr-property/${id}/publish`, payload);
