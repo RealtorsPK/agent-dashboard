9bdd4eb543be7eac474aa1fecdee6fe5cd04a2b5
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-09 16:45:10 +0500

    pagination main

---
+++ components/ui/pagination/index.jsx
@@ -0,0 +1,21 @@
+import ReactPaginate from 'react-paginate';
+
+const Pagination = ({ total, pageSize, onPageChange }) => (
+    <ReactPaginate
+        activeLinkClassName="text-white bg-background-primary"
+        breakLabel="..."
+        breakLinkClassName="flex no-underline w-[40px] h-[40px] text-text-secondary rounded-[4px] bg-[#fff] ml-[5px] mr-[5px] items-center justify-center border-[1px] border-border-primary text-[14px]"
+        className="pl-0 inline-flex w-fit"
+        nextLabel="Next"
+        nextLinkClassName="text-text-primary no-underline flex px-[20px] h-[40px] rounded-[4px] bg-[#fff] ml-[5px] mr-[5px] items-center justify-center border-[1px] border-border-primary text-[14px]"
+      // renderOnZeroPageCount={null}
+        onPageChange={onPageChange}
+        pageCount={Math.ceil(total / pageSize)}
+        pageLinkClassName="flex text-text-primary no-underline w-[40px] h-[40px] rounded-[4px] bg-[#fff] ml-[5px] mr-[5px] items-center justify-center border-[1px] border-border-primary text-[14px]"
+        pageRangeDisplayed={pageSize}
+        previousLabel="Previous"
+        previousLinkClassName="text-text-primary no-underline flex px-[20px] h-[40px] rounded-[4px] bg-[#fff] ml-[5px] mr-[5px] items-center justify-center border-[1px] border-border-primary text-[14px]"
+    />
+  );
+
+export default Pagination;
