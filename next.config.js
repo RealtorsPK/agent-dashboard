3efe623935057d5a8895ccf39d99c113f36458b4
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-13 23:13:08 +0500

    changes

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   async redirects() {
     return [
       {
         source: '/',
-        destination: '/auth/sign-in',
+        destination: '/dashboard',
         permanent: true,
       },
     ]
   },
+  swcMinify: true,
   images: {
     domains: [
       "res.cloudinary.com",
