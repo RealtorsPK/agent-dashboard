9a575719b6ed63f0bbd2d7935c28e6f6ed072c45
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-13 23:12:43 +0500

    changes

--- components/global-components/sidebar/index.jsx
+++ components/global-components/sidebar/index.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 
@@ -21,17 +22,28 @@ const SideBar = () => {
   };
 
   return (
-    <div className="bg-white min-h-[calc(100vh-60px)] py-[20px]">
+    <div className="bg-[#f5f5f5] border-r-[1px] min-h-[calc(100vh)]">
+      <div className="px-[24px] py-[32px]">
+        <span className="relative w-[184px] max-w-[100%] h-[30px] inline-block">
+          <Image
+            alt="Logo Image"
+            fill
+            priority
+            sizes="156px"
+            src="https://dev.realtorspk.com/commercial/_next/static/media/logo.c8203836.svg"
+          />
+        </span>
+      </div>
       {
         buttons.map((items) => (
           <div
-              className={`${toggleDropdown === items.title ? '' : 'h-[41px]'} overflow-hidden transition ease-in-out duration-500`}
-              key={items.title}
+            className={`${toggleDropdown === items.title ? '' : 'h-[36px]'} px-[20px] ${items.subMenu && items.url === active ? 'bg-[#fafafa]' : 'text-[#888888]'} overflow-hidden transition ease-in-out duration-500`}
+            key={items.title}
           >
             <button
-                className={`block w-full text-left text-[14px] py-[10px] px-[20px] ${items.url === active ? 'text-[#000] font-medium' : 'text-[#888888]'}`}
-                onClick={() => items.subMenu ? toggleDropdownFun(items) : goToUrl(items.url, items)}
-                type="button"
+              className={`block w-[100%] text-left text-[14px] py-[7px] px-[15px] pl-[40px] rounded-[6px] ${!items.subMenu && items.url === active ? ' text-text-tertiary bg-[#E5DCF1]' : 'text-text-primary bg-transparent'} hover:text-text-tertiary hover:bg-[#E5DCF1]`}
+              onClick={() => items.subMenu ? toggleDropdownFun(items) : goToUrl(items.url, items)}
+              type="button"
             >
               {items.title}
             </button>
@@ -39,11 +51,11 @@ const SideBar = () => {
             {
               items.subMenu ?
                 items.subMenu.map((subItems) => (
-                  <div key={subItems.title}>
+                  <div className="pl-[40px]" key={subItems.title}>
                     <button
-                        className="block w-full text-left text-[14px] mb-[10px] px-[20px] ml-[15px] text-[#888888]"
-                        onClick={() => goToUrl(subItems.url, items)}
-                        type="button"
+                      className="block w-full text-left text-[14px] py-[7px] px-[15px] text-[#888888] rounded-[6px] hover:text-text-tertiary hover:bg-[#E5DCF1]"
+                      onClick={() => goToUrl(subItems.url, items)}
+                      type="button"
                     >
                       {subItems.title}
                     </button>
