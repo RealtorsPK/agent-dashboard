88e98df622718a8fe69fa8635cc7ca5b84bb0e69
Umer farooq <45381610+umerfarooq69@users.noreply.github.com>
2023-03-13 23:13:31 +0500

    changes

--- payloads/add-property.js
+++ payloads/add-property.js
@@ -1,4 +1,7 @@
-export const addPropertyPayload = (data) => {
+import { pickImgByType, selectBoxSelectedOption } from "../utilities/helper-function";
+import { baths, bedrooms, unitSizes } from "../utilities/static-values";
+
+const addPropertyPayload = (data) => {
   const { title, categoryId, cityId, areaId, rcAgentId, purpose, sizeUnit, size, price, latitude, longitude, description, address, display, cover, image, selectedFeatures, socialLinks, bed, bath } = data;
   let fd = new FormData();
   const youtubeUrls = []
@@ -28,4 +31,48 @@ export const addPropertyPayload = (data) => {
   image && image.length > 0 && image.map((item) => { fd.append('image', item); return null })
 
   return fd;
+}
+
+const editPropertyPayload = (data) => {
+  const { title, area, address, description, city, purpose, category, sizeUnit, size, price, latitude, longitude, resources, bed, bath, features } = data;
+
+  let newFeatures = []
+  features && features.length > 0 && features.map((item) => { return newFeatures.push(item.id) })
+
+  let newSocialLinks = []
+  let getVideoUrls = [...pickImgByType(resources, 'video')]
+  getVideoUrls && getVideoUrls.length > 0 && getVideoUrls.map((item) => { return newSocialLinks.push({ url: item.url, ...item }) })
+
+
+  const payload = {
+    title: title,
+    propertyType: 'residential',
+    address: address,
+    description: description,
+    price: price,
+    purpose: purpose,
+    size: size,
+    latitude: latitude,
+    longitude: longitude,
+    categoryId: category.id,
+    purpose: purpose,
+    sizeUnit: selectBoxSelectedOption(unitSizes, sizeUnit, 'label', 'value'),
+    cityId: { label: city.name, value: city.id },
+    areaId: { label: area.name, value: area.id },
+    bed: selectBoxSelectedOption(bedrooms, bed, 'label', 'value'),
+    bath: selectBoxSelectedOption(baths, bath, 'label', 'value'),
+    cover: pickImgByType(resources, 'cover'),
+    display: pickImgByType(resources, 'display'),
+    image: pickImgByType(resources, 'image'),
+    features: newFeatures,
+    socialLinks: newSocialLinks,
+  }
+
+  return payload;
+
+}
+
+export {
+  addPropertyPayload,
+  editPropertyPayload,
 }
